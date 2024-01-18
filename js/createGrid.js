31ff578604f081d2b305fb90922b4fb7afddf584
Florent <LamacheFlorent>
2024-01-18 19:12:43 +0100

    createGrid.js

---
+++ js/createGrid.js
@@ -0,0 +1,29 @@
+const createGrid = () => {
+    const gridSize = 8;
+
+    const grid = [...Array(gridSize).keys()].map(() => {
+        return [...Array(gridSize).keys()].map(() => {
+            return 0; 
+        });
+    });
+
+    const container = document.querySelector('.game');
+    const html = [];
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            const divEl = document.createElement('div');
+            divEl.classList.add('cell', 'cell-hidden', `cell-row-${row}-col-${col}`);
+            divEl.dataset.rowIndex = row; 
+            divEl.dataset.colIndex = col; 
+
+            container.appendChild(divEl);
+            html.push(divEl);
+        }
+    }
+
+    console.log(grid);
+    return grid;
+};
+
+export default createGrid;
--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 import createGrid from './createGrid.js';
-import addGridElement from './addGridElement.js';
-import addEvent from './addEvent.js';
+//import addGridElement from './addGridElement.js';
+//import addEvent from './addEvent.js';
 
 try {
     // Create grid
     const grid = createGrid();
 
     // Add elements in the grid (mine, number around the mine)
-    addGridElement(grid);
+    //addGridElement(grid);
 
     // Add events
-    addEvent(grid);
+    //addEvent(grid);
     console.table(grid);
 
     // Select restart button
